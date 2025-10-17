6f4ee1a71eb8b6f4fb085f5d627125feae824a9d
sub4ikgg <zerotanker33@gmail.com>
2025-10-18 00:32:49 +0300

    Rename ViewController to MovieQuizViewController

--- ypquiz/MovieQuizViewController.swift
+++ ypquiz/MovieQuizViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieQuizViewController.swift
 //  ypquiz
 //
 //  Created by Кирилл Ефремов on 17.10.2025.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var quizProgressLabel: UILabel!
     @IBOutlet private weak var quizQuestionLabel: UILabel!
     @IBOutlet private weak var quizImageView: UIImageView!
