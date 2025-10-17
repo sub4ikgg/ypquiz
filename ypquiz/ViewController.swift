8d2d9435a6748a9ea424eb09842403b591bbc2f4
Kirill Efremov <kirill.efremov@re-coders.pro>
2025-10-17 16:53:13 +0300

    Initial Commit

---
+++ ypquiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ypquiz
+//
+//  Created by Кирилл Ефремов on 17.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ypquizTests/ypquizTests.swift
@@ -0,0 +1,17 @@
+//
+//  ypquizTests.swift
+//  ypquizTests
+//
+//  Created by Кирилл Ефремов on 17.10.2025.
+//
+
+import Testing
+@testable import ypquiz
+
+struct ypquizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
